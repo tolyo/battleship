8cca3d248c7c709cae28b65c270d39e0935d5b88
Anatoly Ostrovsky <anatolyostrovsky@gmail.com>
2024-01-26 19:40:02 +0200

    Add ships to grid states

--- battleship/src/battleship.hrl
+++ battleship/src/battleship.hrl
@@ -3,7 +3,8 @@
 -define(BLOCKED, 'o').
 -define(HIT, '+').
 -define(MISS, 'x').
--type grid_state() :: ?EMPTY | ?BLOCKED | ?HIT | ?MISS.
+-type grid_state() ::
+    ?EMPTY | ?BLOCKED | ?HIT | ?MISS | '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'.
 -type strike_res() :: 'MISS' | 'HIT' | 'ERROR'.
 -type ship_orientation() :: 'VERTICAL' | 'HORIZONTAL'.
 -type row() :: integer().
